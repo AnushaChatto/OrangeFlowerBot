8b3c8527fd8acdaedc830e7846617e945978181a
AnushaChattopadhyay <jhumpi.1997@gmail.com>
2020-12-21 10:10:42 +0530

    sends random doggo images

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from DISCORD_TOKEN import *
-from random import randrange    
+from random import randrange, choice, seed, uniform
+from datetime import datetime   
+import os 
 
 def get_random_line(afile, default=None):
     line = default
@@ -9,6 +11,9 @@ def get_random_line(afile, default=None):
             line = aline
     return line
 
+# def get_doggo(default=None):
+#     afile = choice(os.listdir("dog-images\\"))
+#     return afile
 
 
 client = discord.Client()
@@ -26,5 +31,32 @@ async def on_message(message):
         with open('greetings.txt') as f:
             greeting = get_random_line(f)
         await message.channel.send(greeting)  
+
+    if message.content.startswith('$time'):
+        now = datetime.now()
+        await message.channel.send(now)
+
+    if message.content.startswith('$are you lonely?'):
+        with open('lonely.txt') as f:
+            quotes = get_random_line(f)
+        await message.channel.send(quotes)
+
+    if message.content.startswith('$are you lonely?'):
+        await message.channel.send("I can't tell you about myself, but this may help you")  
+
+    if message.content.startswith('$Tell me a joke!'):
+        with open('jokes.txt') as f:
+            joke = get_random_line(f)
+        await message.channel.send(joke)  
+
+    if message.content.startswith('$Doggo'):
+        filename = choice(os.listdir("dog-images//"))
+        filepath = ("dog-images/" + filename)
+        await message.channel.send(file=discord.File(filepath))
+
+    if message.content.startswith('$Doggo'):
+        await message.channel.send("Henlo fren")
+        
+
 
 client.run(DISCORD_TOKEN)
